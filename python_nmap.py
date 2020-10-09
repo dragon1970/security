4518ee7e8fd2e6b9467792a2b70ce68f3bda3a06
dragon1970 <2808044788@qq.com>
2020-10-09 22:32:57 +0800

    Add files via upload

---
+++ python_nmap.py
@@ -0,0 +1,9 @@
+"""
+    https://pypi.org/project/python3-nmap/
+    Python3-nmap converts Nmap commands into python3 methods making it very easy to use nmap in any of your python pentesting projects
+
+"""
+
+
+# pip install python3-nmap not work 2020/10/9
+import nmap3
